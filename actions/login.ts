86cca13581d71e7075c131106557c91b63374945
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-03-26 20:09:11 +0530

    🐛

--- actions/login.ts
+++ actions/login.ts
@@ -24,6 +24,8 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
             password,
             redirectTo: DEFAULT_LOGIN_REDIRECT,
         });
+
+        return { success: "Email sent!" };
     } catch (err) {
         if (err instanceof AuthError) {
             switch (err.type) {
